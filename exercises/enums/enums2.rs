fb7b234c716472e48195202b54a209a4b1566071
Dave Banerjee <justdave@linux.com>
2020-12-27 20:58:14 +0000

    more exercises done

--- exercises/enums/enums2.rs
+++ exercises/enums/enums2.rs
@@ -6,11 +6,21 @@
 #[derive(Debug)]
 enum Message {
     // TODO: define the different variants used below
+    Move{ x: i32, y: i32 },
+    Echo(String),
+    ChangeColor(i32, i32, i32),
+    Quit,
 }
 
 impl Message {
     fn call(&self) {
         println!("{:?}", &self);
+        match &self {
+            Message::Move => {x: 10, y: 30},
+            Message::Echo => String::from("hello world"),
+            Message::ChangeColor => ChangeColor(200, 255, 255),
+            Message::Quit,
+        }
     }
 }
 
